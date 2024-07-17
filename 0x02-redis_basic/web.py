bfefbeed7d59dbe391a53a0adf0015538ec9cb7c
brikhotso <brenda.rikhotso@gmail.com>
2024-07-17 23:48:22 +0200

    Increment access count every time the function is called

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,24 +6,27 @@
 import redis
 import requests
 
-
 redi = redis.Redis()
 
-
 def get_page(url: str) -> str:
-    """Fetche HTML content of URL, caches it, and tracks access count"""
+    """Fetch HTML content of URL, caches it, and tracks access count"""
+    # Increment access count every time the function is called
     access_count_key = "count:{}".format(url)
     redi.incr(access_count_key)
+
+    # Check for cached content
     cached_content_key = "cached:{}".format(url)
     cached_content = redi.get(cached_content_key)
 
     if cached_content:
         return cached_content.decode()
 
+    # If not cached, fetch and cache the content
     response = requests.get(url)
     redi.setex(cached_content_key, 10, response.text)
     return response.text
 
-
 if __name__ == "__main__":
-    print(get_page('http://slowwly.robertomurray.co.uk'))
+    url = 'http://slowwly.robertomurray.co.uk'
+    print(get_page(url))
+    print(get_page(url))
