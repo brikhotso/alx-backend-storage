3943579ef60220b5ecba718a52c9144cb8f6b698
brikhotso <brenda.rikhotso@gmail.com>
2024-07-18 00:43:28 +0200

    retun content

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,17 +4,35 @@ Defines a function `get_page`
 """
 import redis
 import requests
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
-def get_page(url: str) -> str:
-    """
-    It uses the requests module to obtain
-    the HTML content of a particular URL and returns it
-    """
-    r = redis.Redis()
-    key = "count:{}{}{}".format('{', url, '}')
-    r.incr(key)
-    res = requests.get(url)
-    r.setex(url, timedelta(seconds=10), res.text)
-    return res.text
+def cache_decorator(func):
+    """"""
+    @wraps(func)
+    def wrapper(url):
+        """
+        Use requests module to obtain
+         HTML content of a particular URL
+        """
+        current_time = datetime.now()
+        # Check if URL is in cache and not expired
+        if url in cache and (current_time - cache[url]['time']).seconds < 10:
+            cache[url]['count'] += 1
+            return cache[url]['content']
+        else:
+            # Fetch new content, reset cache for URL
+            content = func(url)
+            cache[url] = {
+                'content': content,
+                'time': current_time,
+                'count': 1
+            }
+            return content
+    return wrapper
+
+@cache_decorator
+def get_page(url: str) -> str$:
+    """returns HTML content of a particular URL """
+    response = requests.get(url)
+    return response.text
