460db34a76dd826716be6a903b5ff0cd3de4ecce
brikhotso <brenda.rikhotso@gmail.com>
2024-07-18 01:05:42 +0200

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,41 +1,40 @@
 #!/usr/bin/env python3
-"""
-Defines a function `get_page`
-"""
+'''
+    Author  Bezaleel Olakunori
+    A module with tools for request caching and tracking.
+'''
 import redis
 import requests
-from datetime import datetime, timedelta
+from functools import wraps
+from typing import Callable
 
 
-cache = redis.Redis()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def cache_decorator(func):
-    """"""
-    @wraps(func)
-    def wrapper(url):
-        """
-        Use requests module to obtain
-         HTML content of a particular URL
-        """
-        current_time = datetime.now()
-        # Check if URL is in cache and not expired
-        if url in cache and (current_time - cache[url]['time']).seconds < 10:
-            cache[url]['count'] += 1
-            return cache[url]['content']
-        else:
-            # Fetch new content, reset cache for URL
-            content = func(url)
-            cache[url] = {
-                'content': content,
-                'time': current_time,
-                'count': 1
-            }
-            return content
-    return wrapper
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
-@cache_decorator
-def get_page(url: str) -> str$:
-    """returns HTML content of a particular URL """
-    response = requests.get(url)
-    return response.text
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
